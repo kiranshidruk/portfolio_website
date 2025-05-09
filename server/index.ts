74d97c1c8e60a35c31130f9ae75dece24acaddec
kiranshidruk <kiranshidruk27@gmail.com>
2025-05-09 19:06:52 -0400

    changes in index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,14 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import path from "path";
+import { fileURLToPath } from 'url';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Catch-all route for SPA
