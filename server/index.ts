fbe2991c988c5c06846702a773c73c597e32318f
kiranshidruk <kiranshidruk27@gmail.com>
2025-05-09 19:01:01 -0400

    change for render issue

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,17 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import path from "path";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Catch-all route for SPA
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.use((req, res, next) => {
   const start = Date.now();
